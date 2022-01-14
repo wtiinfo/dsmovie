edad8359c7aa676d5825ec1a246b8e5b083cef98
wtiinfo <wanbrwan@gmail.com>
2022-01-14 20:00:50 -0200

    Teste

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar() {
         <header>
             <nav className='container'>
                 <div className='dsmovie-nav-content'>
-                    <h1>DSMovie</h1>
+                    <h1>DSMovie-Wtiinfo</h1>
                     <a {...meus_links}>
                         <div className='dsmovie-contact-container'>
                             <GithubIcon />
